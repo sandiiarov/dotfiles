84423e64e657bcda4c194ec878086e601127d641
sandiiarov <sandiyarov@gmail.com>
2024-06-09 22:54:37 -0400

    upd

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -48,17 +48,6 @@ return {
           fallback()
         end
       end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif vim.snippet.active({ direction = -1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(-1)
-          end)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
     })
     opts.experimental = {
       ghost_text = false,
