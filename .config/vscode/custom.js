1762145b7f3096ce381fb2612e20eec181bfa900
Alex Sandiiarov <sandiyarov@gmail.com>
2024-07-27 15:44:09 -0400

    Update custom.js

--- .config/vscode/custom.js
+++ .config/vscode/custom.js
@@ -1,18 +1,36 @@
+let isPrettified = false;
+
 const observer = new MutationObserver(() => {
-  if (document.title === "lazygit") {
+  const tabs = document.querySelector(".tabs");
+  const footer = document.querySelector(".split-view-view .statusbar");
+
+  if (!isPrettified && (window.screenY > 0 || window.screenX > 0)) {
     window.resizeTo(window.screen.availWidth - 200, window.screen.availHeight - 100);
     window.moveTo(100, 50);
 
-    const tabs = document.querySelector(".tabs");
     if (tabs) {
       tabs.style.opacity = "0";
       tabs.style.height = "20px";
     }
 
-    const footer = document.querySelector(".split-view-view .statusbar");
     if (footer) {
       footer.style.opacity = "0";
     }
+
+    isPrettified = true;
+  }
+
+  if (isPrettified && window.screenY === 0 && window.screenX === 0) {
+    if (tabs) {
+      tabs.style.opacity = "1";
+      tabs.style.height = "28px";
+    }
+
+    if (footer) {
+      footer.style.opacity = "1";
+    }
+
+    isPrettified = false;
   }
 });
 
