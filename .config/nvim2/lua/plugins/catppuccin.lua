b89bfb3b51e553a743c051bdde64e98e3d563dec
Alex Sandiiarov <sandiyarov@gmail.com>
2024-10-19 16:49:52 -0400

    Update catppuccin.lua

--- .config/nvim2/lua/plugins/catppuccin.lua
+++ .config/nvim2/lua/plugins/catppuccin.lua
@@ -5,47 +5,47 @@ return {
       transparent_background = true,
     },
   },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   opts = {
-  --     window = {
-  --       completion = {
-  --         border = border("CmpBorder"),
-  --         winhighlight = "Normal:MyHighlight,FloatBorder:CmpBorder,CursorLine:Visual,Search:None",
-  --         blend = 0,
-  --       },
-  --       documentation = {
-  --         border = border("CmpBorder"),
-  --         winhighlight = "Normal:Normal,FloatBorder:CmpBorder,CursorLine:Visual,Search:None",
-  --         blend = 0,
-  --       },
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ui = {
-  --       border = border("MasonBorder"),
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "folke/noice.nvim",
-  --   opts = {
-  --     presets = {
-  --       lsp_doc_border = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     diagnostics = {
-  --       float = {
-  --         border = border("LspFloatBorder"),
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.window = {
+        completion = {
+          border = "rounded",
+          winhighlight = "Normal:MyHighlight",
+          winblend = 0,
+        },
+        documentation = {
+          border = "rounded",
+          winhighlight = "Normal:MyHighlight",
+          winblend = 0,
+        },
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        float = {
+          border = "rounded",
+        },
+      },
+    },
+  },
 }
