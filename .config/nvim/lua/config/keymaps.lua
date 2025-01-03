6ff6db6049be9d8c1cde88e1e2fe02c779d7cd30
sandiiarov <sandiyarov@gmail.com>
2025-01-03 10:02:31 -0500

    upd

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,9 +8,3 @@ map("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-S-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-S-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-
--- TMUX
-vim.keymap.set("n", "<C-h>", ":<C-U>NvimTmuxNavigateLeft<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-l>", ":<C-U>NvimTmuxNavigateRight<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-j>", ":<C-U>NvimTmuxNavigateDown<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", ":<C-U>NvimTmuxNavigateUp<cr>", { noremap = true, silent = true })
