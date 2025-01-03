545a25486e7ab30923b3e203d45529a2c338bf55
sandiiarov <sandiyarov@gmail.com>
2025-01-03 10:00:54 -0500

    upd

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
-    { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
