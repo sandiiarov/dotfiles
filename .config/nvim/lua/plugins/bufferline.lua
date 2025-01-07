44c099ba9332dcf6a6eb7420e4bd98d9e76e537a
N780016 <aleksandr.sandiiarov@jpmchase.com>
2025-01-07 11:34:01 -0500

    upd

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      always_show_bufferline = true,
+      separator_style = { "", "" },
+      indicator = {
+        icon = ".",
+        style = "icon",
+      },
+      modified_icon = "",
+    },
+  },
+}
