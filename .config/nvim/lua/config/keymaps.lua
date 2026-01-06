ae1c4d753d7476a3c2c99495a57a4518235cf35f
alexsandiiarov <sandiiarov@csdisco.com>
2026-01-06 13:24:25 -0500

    upd

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,11 +3,5 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 
--- Resize window using <ctrl> arrow keys
-map("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-S-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-S-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-
 -- Restart LSP
 map("n", "<leader>cq", "<cmd>LspRestart<cr>", { desc = "Lsp Restart" })
